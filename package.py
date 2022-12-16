acbc3fcc8c0ecd40af4704a1e235dc1cb3dff560
Hibiki Suzuki <h.suzuki@guncys.com>
2022-12-16 16:40:04 +0900

    package.pyの追加

---
+++ package.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+name = 'studiolibrary'
+
+version = '2.9.6.b3'
+
+requires = ['maya']
+
+private_build_requires = ['rezutil-1']
+build_command = "python -m rezutil build {root}"
+
+def commands():
+    global env
+    
+    env.PYTHONPATH.append("{root}/src")
